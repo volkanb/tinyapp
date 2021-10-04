58c36ef7bc3f81b7703c2fd70a645204b9c0a571
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-04 01:53:59 -0400

    Adds register endpoint with the view

--- express_server.js
+++ express_server.js
@@ -59,6 +59,14 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
+  res.render("register", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
