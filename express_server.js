ab90172a8082cd78710026864e7767a65670a3a9
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-03 23:03:55 -0400

    Adds generateRandomString function

--- express_server.js
+++ express_server.js
@@ -12,6 +12,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
