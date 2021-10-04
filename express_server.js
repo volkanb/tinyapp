3bdf246229f657af82872990a09f6893e3c4506f
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-04 01:35:25 -0400

    Integrate cookie parser

--- express_server.js
+++ express_server.js
@@ -7,6 +7,9 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -23,6 +26,12 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  console.log('Login attempt from user: ' + req.body.username);
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -46,16 +55,27 @@ app.post("/urls/:shortURL/update", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']
+  };
   res.render("urls_show", templateVars);
 });
 
